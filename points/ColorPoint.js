897fc7fcc6914f79e484d3b6543d08b5add29740
Shahid J <shahid@mralbert.se>
2017-05-22 17:10:14 +0200

    Init

---
+++ points/ColorPoint.js
@@ -0,0 +1,17 @@
+
+import Point from './Point';
+
+import React from 'react';
+
+
+
+export default class ColorPoint extends Point {
+    constructor(x, y, color) {
+        super(x, y);
+        this.color = color;
+    }
+    toString() {
+        return super.toString() + ' in ' + this.color;
+    }
+}
+
